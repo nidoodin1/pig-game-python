b8222572e7a27df238849c00f838ad28b439d86b
nidoodin1 <79559583+nidoodin1@users.noreply.github.com>
2021-03-10 13:50:06 +0800

    Update pig game python.py

--- pig game python.py
+++ pig game python.py
@@ -1,21 +1,35 @@
-import random
+input random
+
+player1 = True
+player2 = False
 
 def roll_one():
-
+    if player1 == True:
+        play2()
+    if player2 == True
+        play1()
+        
 def roll_good():
 
-def roll_hold():#odin
+def hold():#odin
+    if player1 == True
+        total_points1 += ponts
+        
+    if player2 == True
+        total_points2 += ponts
 
 def play1():#odin
+    player1 = True
+    player2 = False
 
 def play2():#odin
-
-def trc():
+    player1 = False
+    player2 = True
 
 def win():
+    if total_ponts1 => 100:
+        print ("player 1 wins")
+    if total_ponts2 => 100:
+        print ("player 2 wins")
 
-roll = random.randint(1, 6) # Roll dice
-if (roll == 1):
-    roll_one()
-else:
-    roll_good()
+def trc():
