1ab8dc9af2fd56719cd273d2176d734de0146462
nidoodin1 <79559583+nidoodin1@users.noreply.github.com>
2021-03-17 14:13:19 +0800

    Update pig game python.py

--- pig game python.py
+++ pig game python.py
@@ -1,25 +1,12 @@
 #setup
-import random
+import random#for dice
 
-num = 0
-player = True
-point1 = []
-point2 = []
-temp_point = []
-player1_history = []
-player2_history = []
-
-def listToString(s):  
-    
-    # initialize an empty string 
-    str1 = ""  
-    
-    # traverse in the string   
-    for ele in s:  
-        str1 += ele   
-    
-    # return string   
-    return str1 
+num = 0 #empty number for math
+player = True #True = player 1 False = player 2
+point1 = [] #player 1 points
+point2 = [] #player 2 points
+temp_point = [] #ponints that can be lost
+to_win = 10 #how menny you need to win
 
 def roll():#roll the dice random number 1-6
     global num
@@ -27,48 +14,36 @@ def roll():#roll the dice random number 1-6
     print (num)
     return num
 
-def roll_bad():#rolled a 1 swich player lose temp points
+def roll_bad():#if you rolled a 1 swich player lose temp points
     global temp_point
     print ("you fail")
     temp_point.clear()
-    print(temp_point)
     swap()
 
-
-def roll_good(num):#rolld more then one add to temp points
-    global temp_point
-    
+def roll_good(num):#add roll to temp points and have anuther roll
+    global temp_point   
     temp_point.append(num)
     
-
-
-def hold():#cash out add temp points to points change turn
+def hold():#cash out and add temp points to true points temp points now empty
     if player == True:
-        #print ("player 1 turn")
-        #print(temp_point)
         point1.extend(temp_point)
         temp_point.clear()
         print(point1)
     elif player == False:
-        #print ("player 2 turn")
         point2.extend(temp_point)
         temp_point.clear()
         print(point2)
 
-    swap()
-
-
-def win():# check if a player wins
-    if sum(point1) > 10:
+def win():# check if a player wins #end program hear
+    if sum(point1) > to_win:
         print ("player 1 wins")
-        quit()
-    if sum(point2) > 10:
+        return True
+    if sum(point2) > to_win:
         print ("player 2 wins")
-        quit()
+        return True
 
-def swap():
-    global player
-    
+def swap():#chang turn
+    global player  
     if player == True:
         player = False
         temp_point.clear()
@@ -78,15 +53,14 @@ def swap():
         temp_point.clear()
         print ("player 1 turn")
 
-def see():
+def see():#see how menny points
     print("player 1" , point1)
     print("player 2" , point2)
 
-while True:
-    win()
-    pick = input ("roll or hold\n")
+while True:#main loop
+    pick = input ("roll\nhold\nsee\n")#input
 
-    if pick == "roll":
+    if pick == "roll":#pick roll
         got = roll()
         if got != 1:
             roll_good(got)
@@ -94,16 +68,20 @@ while True:
         else:
             roll_bad()
             
-    elif pick == "hold":
+    elif pick == "hold":#pick hold
         hold()
+        if win() == True:
+            break
+        swap()
 
-    elif pick == "see":
+    elif pick == "see":#pick see
         see()
 
-    else:
+    else:#pick error try again
         print("input error")
 
-    
+
+print("\n-----end of program-----\n") 
 
   
 
