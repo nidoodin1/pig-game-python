4a730c22f2d512c5dadaf09ffb9828e408a3f892
nidoodin <20049115@tafe.wa.edu.au>
2021-05-12 13:06:10 +0800

    added team colers

--- pig game python.py
+++ pig game python.py
@@ -1,11 +1,17 @@
-import random # For dice rolls
 
+import random  # For dice rolls
+import colored
+from colored import fg, attr, stylize
+
+color = colored.fg("blue")
 num = 0
-player = True # True = Player 1, False = Player 2
-point1 = [] # Points for Player 1
-point2 = [] # Points for Player 2
-temp_point = [] # Points that can be lost
-to_win = 30 # Points required to win
+player = True  # True = Player 1, False = Player 2
+point1 = []  # Points for Player 1
+point2 = []  # Points for Player 2
+temp_point = []  # Points that can be lost
+to_win = 15  # Points required to win
+pink = colored.fg("hot_pink_3a")
+
 
 def roll():
     """ Rolls 6 sided dice """
@@ -13,54 +19,64 @@ def roll():
     num = random.randrange(1, 6)
     return num
 
+
 def roll_bad():
     """ Event for bad roll """
     global temp_point
-    print ("You rolled a 1, next player's turn")
+    print(stylize("You rolled a 1, next player's turn", color))
     temp_point.clear()
     swap()
 
+
 def roll_good(num):
     """ Event for good roll """
-    global temp_point   
+    global temp_point
     temp_point.append(num)
-    
+
+
 def hold():
     """ Cash out points into table """
     if player == True:
         point1.extend(temp_point)
         temp_point.clear()
-        print(point1)
+        print(color + "", point1)
     elif player == False:
         point2.extend(temp_point)
         temp_point.clear()
-        print(point2)
+        print(color + "", point2)
+
 
 def win():
     """ Checks for win by either player """
     if sum(point1) > to_win:
-        print("Player 1 wins")
+        print(stylize("Player 1 wins", color))
         return True
     if sum(point2) > to_win:
-        print("Player 2 wins")
+        print(stylize("Player 2 wins", color))
         return True
+
 
 def swap():
     """ Swap player turn """
-    global player  
+    global player
+    global color
     if player == True:
         player = False
         temp_point.clear()
-        print ("Player 2's turn")
-    else: #player == False:
+        color = colored.fg("red")
+        print(stylize("Player 2's turn", color))
+    else:  # player == False:
         player = True
         temp_point.clear()
-        print ("Player 1's turn")
+        color = colored.fg("blue")
+        print(stylize("Player 1's turn", color))
+
 
 def see():
     """ Display number of points for both players """
-    print("Player 1 points:", point1)
-    print("Player 2 points:", point2)
+    print(color + "Player 1 points:", point1)
+    print(color + "Player 2 points:", point2)
+
 
 def helpme():
     """ Display help """
@@ -68,7 +84,7 @@ def helpme():
 
 
 print("Type 'help' for help")
-print("Player 1's turn")
+print(stylize("Player 1's turn", color))
 
 # Main loop
 while True:
@@ -78,12 +94,12 @@ while True:
     # Roll picked
     if pick == "roll":
         result = roll()
-        print("You rolled:", result)
+        print(color + "You rolled:", result)
         if result != 1:
             roll_good(result)
         else:
             roll_bad()
-            
+
     # Hold picked
     elif pick == "hold":
         hold()
@@ -104,12 +120,7 @@ while True:
         print("Wrong input, type 'help' for commands")
 
 see()
-print("\n-----end of program-----\n") 
-
-  
-
-    
-       
+print(stylize("\n-----end of program-----\n", pink))
 
 
 
@@ -124,7 +135,3 @@ print("\n-----end of program-----\n")
 
 
 
-
-
-
-    
